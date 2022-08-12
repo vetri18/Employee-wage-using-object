b15de50822d1c9a339237e7deaa0809a00ac4bc1
vetrivel <vetrivel.kce@gmail.com>
2022-08-12 12:27:46 +0530

    uc1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,10 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employeewage program");
+            Console.WriteLine("Welcome to Employeewage program.");
+
+            CheckEmployee checkEmployee = new CheckEmployee();
+            checkEmployee.CheckEmployeeAttendence();
         }
     }
 }
